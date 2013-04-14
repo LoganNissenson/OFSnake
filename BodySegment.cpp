087385c37cdb209c52876a8d6aec24d15d220986
LoganNissenson <logannissenson@gmail.com>
2013-04-14 16:47:09 -0600

    Create BodySegment.cpp

---
+++ BodySegment.cpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "BodySegment.h"
+#include "FoodMorsel.h"
+#include "ofGraphics.h"
+
+using namespace std;
+
+class Snake
+{
+public:
+  BodySegment head;
+	
+	Snake(int startLength, double initialHeadX, double initialHeadY);
+
+	void eat(FoodMorsel foodJustEaten);
+	void setXDirection(int newXDirection);
+	void setYDirection(int newYDirection);
+	int getYDirection();
+	int getXDirection();
+	void move();
+	BodySegment getSegmentAt(int index);
+	int getBodyLength();
+
+private:
+	int x_direction;			// Varies between 1, and -1
+	int y_direction;			// Varies between 1, and -1
+	vector<BodySegment> body;	
+};
+
